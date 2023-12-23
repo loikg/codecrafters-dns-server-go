cc23caede92b91f50a8d69fa4f4031ec37b20cc8
loikg <mail@company.com>
2023-12-23 15:33:20 +0800

    stage 4 add unit tests

--- app/main_test.go
+++ app/main_test.go
@@ -31,6 +31,7 @@ func TestDNSMessage_Serialize(t *testing.T) {
                     Flags: DNSHeaderFlags{
                         QR: true,
                     },
+                    QDCOUNT: 1,
                 },
                 Questions: []DNSQuestion{
                     {
@@ -41,9 +42,48 @@ func TestDNSMessage_Serialize(t *testing.T) {
                 },
             },
             expected : []byte{
-                0x04,0xD2, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Header
+                0x04,0xD2, 0x80, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Header
                 0x06, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d, 0x00, // Question
                 0x00, 0x01, 0x00, 0x01,
+            },
+        },
+        {
+            name: "serialize answer section in correct binary representation",
+            msg:    DNSMessage{
+                Header: DNSHeader{
+                    ID: 1234, 
+                    Flags: DNSHeaderFlags{
+                        QR: true,
+                    },
+                    ANCOUNT: 1,
+                },
+                Questions: []DNSQuestion{
+                    {
+                        Name: "google.com" ,
+                        Type: 0x01,
+                        Class: 0x01,
+                    },
+                },
+                Answers: []DNSAnswer{
+                    {
+                        Name: "google.com",
+                        Type: 1,
+                        Class: 1,
+                        TTL: 60,
+                        Data: []byte{0x8, 0x8, 0x8, 0x8},
+                    },
+                },
+            },
+            expected : []byte{
+                // Header
+                0x04,0xD2, 0x80, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
+                // Questions
+                0x06, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d, 0x00,
+                0x00, 0x01, 0x00, 0x01,
+                // Answers
+                0x06, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d, 0x00,
+                0x0, 0x01, 0x0, 0x01, 0x0, 0x0, 0x0, 0x3C, 0x0, 0x04, 0x08, 0x08, 0x08,
+                0x08,
             },
         },
     }
